5e035b47cec31c03b1eae9128b35a678338ed540
ozzlamaz <a.osman4233@gmail.com>
2023-08-25 19:34:51 +0300

    initial design

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import buttonMap from './data'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='numpad'>
+      <div id='display'>0</div>
+      <h1 id='title'>Calculator</h1>
+      <h6 id='author'>by Ahmad Osman</h6>
+      {buttonMap.map(button => {
+        const {sign, id, elementID} = button;
+        return (
+          <button key={id} className='button' id={elementID} type="button">{sign}</button>
+        )
+      })}
+    </div>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,124 @@
+const buttonMap = 
+[
+    {
+        sign: 'C',
+        number: false,
+        operator: 'false',
+        id: 1,
+        elementID: 'clear'
+    },
+    {
+        sign: '÷',
+        number: false,
+        operator: true,
+        id: 2,
+        elementID: 'divide'
+    },
+    {
+        sign: '7',
+        number: true,
+        operator: false,
+        id: 3,
+        elementID: 'seven'
+    },
+    {
+        sign: '8',
+        number: true,
+        operator: false,
+        id: 4,
+        elementID: 'eight'
+    },
+    {
+        sign: '9',
+        number: true,
+        operator: false,
+        id: 5,
+        elementID: 'nine'
+    },
+    {
+        sign: 'x',
+        number: false,
+        operator: true,
+        id: 6,
+        elementID: 'multiply'
+    },
+    {
+        sign: '4',
+        number: true,
+        operator: false,
+        id: 7,
+        elementID: 'four'
+    },
+    {
+        sign: '5',
+        number: true,
+        operator: false,
+        id: 8,
+        elementID: 'five'
+    },
+    {
+        sign: '6',
+        number: true,
+        operator: false,
+        id: 9,
+        elementID: 'six'
+    },
+    {
+        sign: '-',
+        number: false,
+        operator: true,
+        id: 10,
+        elementID: 'substract'
+    },
+    {
+        sign: '1',
+        number: true,
+        operator: false,
+        id: 11,
+        elementID: 'one'
+    },
+    {
+        sign: '2',
+        number: true,
+        operator: false,
+        id: 12,
+        elementID: 'two'
+    },
+    {
+        sign: '3',
+        number: true,
+        operator: false,
+        id: 13,
+        elementID: 'three'
+    },
+    {
+        sign: '+',
+        number: false,
+        operator: true,
+        id: 14,
+        elementID: 'add'
+    },
+    {
+        sign: '0',
+        number: true,
+        operator: false,
+        id: 15,
+        elementID: 'zero'
+    },
+    {
+        sign: '.',
+        number: false,
+        operator: false,
+        id: 16,
+        elementID: 'decimal'
+    },
+    {
+        sign: '=',
+        number: false,
+        operator: false,
+        id: 17,
+        elementID: 'equals'
+    },
+]
+
+export default buttonMap;
