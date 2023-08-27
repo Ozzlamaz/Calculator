fec23528eaff9709fc15161036f32e5254d40677
ozzlamaz <a.osman4233@gmail.com>
2023-08-27 08:06:56 +0300

    Redesign display

--- src/App.jsx
+++ src/App.jsx
@@ -11,31 +11,27 @@ const checkminus = /([-]$)/;
 function App() {
   
   const [result, setResult] = useState('0'); 
-  const [evalResult, setEvalResult] = useState('0');
+  const [history, setHistory] = useState('');
 
   const updateDisplay = (sign) => {
     
     if(sign == 'C') {
-      console.log(sign);
       setResult('0');
-      setEvalResult('0');
-      document.querySelector('.result').classList.remove('hide');
-      document.querySelector('.evalresult').classList.add('hide');
+      setHistory('');
+      // document.querySelector('.result').classList.remove('hide');
+      // document.querySelector('.history').classList.add('hide');
       return;
     }
 
-    if (result == '0' && !endsWithNumber.test(sign) && evalResult == '0') {
-      console.log(sign);
+    if (result == '0' && !endsWithNumber.test(sign) && history == '0') {
       return;
     }
 
     if (checkDecimal.test(result) && sign == '.') {
-      console.log(sign);
       return;
     }
 
-    if (endsWithZero.test(result) && !checkZero.test(result) && sign != '.' && evalResult == '0') {
-        console.log(sign);
+    if (endsWithZero.test(result) && !checkZero.test(result) && sign != '.' && history == '0') {
         let newResult = result.slice(0, result.length - 1) + sign;
         setResult(newResult);
         return;
@@ -43,53 +39,51 @@ function App() {
 
     if (!endsWithNumber.test(result) && !endsWithNumber.test(sign)) {
       
-      if(evalResult != '0') {
-        console.log(sign);
-        setResult(evalResult);
-        setEvalResult('0');
-        document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
+      if(history != '0') {
+        setResult(history);
+        setHistory('0');
+        // document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
       }
       if(sign != '-') {
-        console.log(sign);
         let newResult = result.replace(/[\D]+$/, sign);
         setResult(newResult);
         return;
       }
       if (checkminus.test(result)) {
-        console.log(sign);
         return;
       }
     }
 
     if(sign == '=') {
-      document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
-      setEvalResult(eval(result));
+      // document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
+      setHistory(eval(result));
       setResult('');
-      return evalResult;
+      return history;
     }
-    if(evalResult != '0') {;
-      console.log(sign);
-      setResult(evalResult + sign);
-      setEvalResult('0');
-      document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
+    if(history != '0') {;
+      // document.querySelectorAll('.results').forEach((span => span.classList.toggle('hide')));
+      setResult(history + sign);
+      setHistory('0');
       return;
     }
-    console.log(sign);
     setResult(result + sign)
   }
 
   return (
-    <div className='numpad'>
-      <div id='display'><span className='evalresult results hide'>{evalResult}</span><span className='result results'>{result}</span></div>
+    <>
       <h1 id='title'>Calculator</h1>
+      <div className='numpad'>
+        <div id='formula'>{history}</div>
+        <div id='display'>{result}</div>
+        {buttonMap.map(button => {
+          const {sign, id, elementID} = button;
+          return (
+            <button onClick={() => {updateDisplay(sign)}} key={id} className='button' id={elementID} type="button">{sign}</button>
+          )
+        })}
+      </div>
       <h6 id='author'>by Ahmad Osman</h6>
-      {buttonMap.map(button => {
-        const {sign, id, elementID} = button;
-        return (
-          <button onClick={() => {updateDisplay(sign)}} key={id} className='button' id={elementID} type="button">{sign}</button>
-        )
-      })}
-    </div>
+    </>
   )
 }
 
